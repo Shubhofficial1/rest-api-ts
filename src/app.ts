86553f445777e706505a33d2dc44d3af676f6468
shubham kumar <shubhofficial1@gmail.com>
2024-03-23 17:54:08 +0530

    Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const middleware =
+  ({ name }: { name: string }) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    //   @ts-ignore
+    req.user = name;
+    next();
+  };
+
+app.use(middleware({ name: "Shubham1" }));
 
 app.get("/health", (req: Request, res: Response) => {
   return res.send({
@@ -13,6 +23,8 @@ app.get("/health", (req: Request, res: Response) => {
 
 app.get("/api/books/:bookId/:authorId", (req: Request, res: Response) => {
   console.log(req.params);
+  // @ts-ignore
+  console.log(req.user);
   return res.send(req.params);
 });
 
