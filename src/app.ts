55d3ce774e611df02ade3dd528d841721183ba0b
shubham kumar <shubhofficial1@gmail.com>
2024-03-23 15:54:25 +0530

    routing

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
   return res.send("Hello World");
+});
+
+app.post("/api/data", (req: Request, res: Response) => {
+  console.log(req.body);
+  return res.sendStatus(200);
+});
+
+app.all("/api/all", (req: Request, res: Response) => {
+  return res.send("You made a req to all http methods");
 });
 
 app.listen(5000, () => {
